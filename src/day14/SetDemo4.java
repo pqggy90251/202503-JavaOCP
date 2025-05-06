18d6ab534e24557652a995f2559a05cc9eba34e1
pqggy90251 <pqggy90251>
2025-05-06 20:36:29 +0800

    update

---
+++ src/day14/SetDemo4.java
@@ -0,0 +1,15 @@
+package day14;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo4 {
+
+	public static void main(String[] args) {
+		Set<Task> dailyTasks=new LinkedHashSet<>();
+		dailyTasks.add(new Task("05:30", "起床盥洗"));
+		
+
+	}
+
+}
